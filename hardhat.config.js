376e2f01a77fcc4f5b2a23f0ce98744b69cabde4
ngocban <daongocban1102@gmail.com>
2022-10-21 10:19:29 +0700

    scatterplot completed and update readme

--- hardhat.config.js
+++ hardhat.config.js
@@ -124,28 +124,7 @@ module.exports = {
         url: "https://hardcore-mayer:untrue-puppet-yearly-early-widow-spud@nd-723-346-173.p2pify.com",
       },
       chainId: 97,
-      accounts: [
-        {
-          privateKey:
-            "0x36f1ea3519a6949576c242d927dd0c74650554cdfaedbcd03fb3a80c558c03de",
-          balance: "100000000000000000000000000000",
-        },
-        {
-          privateKey:
-            "0xa0c31ec3759513cbdcb60bd0d3f30d298bcede28c06c5dd3b77b2b8219158de6",
-          balance: "100000000000000000000000000000",
-        },
-        {
-          privateKey:
-            "0x850da640e20869c7760ad91c41f7d8e5964403119a0a0f5fdd7a3340b701878e",
-          balance: "100000000000000000000000000000",
-        },
-        {
-          privateKey:
-            "0xee2872a87e48507961714c06d9929e125379e5eda406824e671952257d5e9cd9",
-          balance: "100000000000000000000000000000",
-        },
-      ],
+      accounts: [],
       gasPrice: 20e9,
       gas: 25e6,
     },
@@ -153,10 +132,7 @@ module.exports = {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       chainId: 97,
       gasPrice: 20000000000,
-      accounts: [
-        "0x36f1ea3519a6949576c242d927dd0c74650554cdfaedbcd03fb3a80c558c03de",
-        "0xa0c31ec3759513cbdcb60bd0d3f30d298bcede28c06c5dd3b77b2b8219158de6",
-      ],
+      accounts: [],
     },
   },
 };
