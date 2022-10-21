4b4194571fac2c5e013275378cf8d665200c9e08
ngocban <daongocban1102@gmail.com>
2022-10-21 09:45:04 +0700

    scatterplot completed

--- analysis/index.py
+++ analysis/index.py
@@ -1,5 +1,7 @@
 import resource
 import pymongo
+import matplotlib.pyplot as plt
+import numpy as np
 
 # connect to mongodb
 connection = pymongo.MongoClient("mongodb+srv://NgocBan:Ban280401@cluster0.twmss.mongodb.net/retryWrites=true&w=majority")
@@ -17,17 +19,21 @@ def printCollection():
 def getResource():
     list = []
     for x in collections.find():
-        list.append([x['requestId'], x['responseTimestamp'] - x['requestTimestamp'] ])
+        list.append([x['requestId'],x['responseTimestamp'] - x['requestTimestamp'] ])
     return list
 
- 
+# scatterplot with list data
+def scatterplot(list):
+    x = np.array(list)
+    plt.scatter(x[:,0], x[:,1])
+    plt.show()
+    
     
  
 # main 
 if __name__ == "__main__":
     # printCollection()
     list = getResource()
-    for x in list:
-        print(x)
+    scatterplot(list)
        
     
